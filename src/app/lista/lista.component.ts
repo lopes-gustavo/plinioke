f2d29dc6023504152ad139e31a40dbf8b6432c44
Gustavo Lopes <gustavo.lopes@engenharia.ufjf.br>
2019-12-31 13:35:36 -0300

    Cria StorageService para salvar os favoritos

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { MusicaRecord, MusicasService } from '../services/musicas.service';
+import { StorageService } from '../services/storage.service';
 
 
 @Component({
@@ -9,15 +10,28 @@ import { MusicaRecord, MusicasService } from '../services/musicas.service';
   styleUrls: [ './lista.component.scss' ]
 })
 export class ListaComponent implements OnInit {
+  private favoriteList = {};
   musicaRecords = this.musicasService.allMusicas;
 
   constructor(
     private musicasService: MusicasService,
+    private storageService: StorageService,
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.favoriteList = this.storageService.get('favorite') || {};
+    this.musicaRecords.forEach(record => record.favorite = this.favoriteList[record.code] === true);
+  }
 
   changeFavorite(record: MusicaRecord) {
     record.favorite = !record.favorite;
+
+    if (record.favorite) {
+      this.favoriteList[record.code] = true;
+    } else {
+      delete this.favoriteList[record.code];
+    }
+
+    this.storageService.save('favorite', this.favoriteList);
   }
 }
---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+
+const getWindow = () => window;
+
+@Injectable({ providedIn: 'root' })
+export class StorageService {
+  private seed = '__PLINIOKE__';
+  private storage = getWindow().localStorage;
+
+  public get<T>(key: string) {
+    return JSON.parse(this.storage.getItem(this.seed + key)) as T;
+  }
+
+  public save(key: string, value: any) {
+    const strValue = JSON.stringify(value);
+    this.storage.setItem(this.seed + key, strValue);
+  }
+}
