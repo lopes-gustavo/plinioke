76c9aff9b8e48c3db2b52f67595c49dd6417db97
Gustavo Lopes <gustavo.lopes@engenharia.ufjf.br>
2020-01-01 20:23:53 -0300

    Adiciona refresh automático quando houver nova versão do PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 
 @Component({
@@ -6,4 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private swUpdate: SwUpdate) {
+    this.configureNewVersionAvailableEvent();
+  }
+
+  private configureNewVersionAvailableEvent() {
+    this.swUpdate.available.subscribe(event => {
+      console.log('current version is', event.current);
+      console.log('available version is', event.available);
+
+      this.swUpdate.activateUpdate().then(() => {
+        document.location.reload();
+        console.log('The app is updating right now');
+      });
+    });
+
+    this.swUpdate.activated.subscribe(event => {
+      console.log('old version was', event.previous);
+      console.log('new version is', event.current);
+    });
+
+  }
+}
