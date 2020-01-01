0b6e3d51d9c800e4e854586efe866de96ae6de35
Gustavo Lopes <gustavo.lopes@engenharia.ufjf.br>
2020-01-01 20:26:24 -0300

    Cria rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ListaDeMusicasComponent } from './lista-de-musicas/lista-de-musicas.component';
+import { ShareComponent } from './share/share.component';
+import { MyListsComponent } from './my-lists/my-lists.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: ListaDeMusicasComponent, pathMatch: 'full' },
+  { path: 'compartilhar', component: ShareComponent },
+  { path: 'my-lists', component: MyListsComponent },
+  { path: 'sobre', component: AboutComponent },
+];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 import { environment } from '../environments/environment';
 
@@ -12,25 +15,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaDeMusicasComponent } from './lista-de-musicas/lista-de-musicas.component';
 import { FilterPipe } from './pipes/filter.pipe';
-import { ScrollingModule } from '@angular/cdk/scrolling';
 import { FavFilterPipe } from './pipes/favFilter.pipe';
+import { MapPipe } from './pipes/map.pipe';
+import { ShareComponent } from './share/share.component';
+import { MyListsComponent } from './my-lists/my-lists.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaDeMusicasComponent,
     FilterPipe,
     FavFilterPipe,
-    ListaComponent,
+    MapPipe,
+    ListaDeMusicasComponent,
+    ShareComponent,
+    MyListsComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     BrowserAnimationsModule,
     ScrollingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     MDBBootstrapModule.forRoot(),
     FormsModule,
+    HttpClientModule,
     AppRoutingModule,
   ],
   providers: [],
